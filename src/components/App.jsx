9f7e486e9f32fcb6ea806d0adc32a542d7269a53
AnkA852456 <108530192+AnkA852456@users.noreply.github.com>
2023-11-24 20:04:19 +0200

    2nd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import { Profile } from './Profile/Profile';
 import user from '../user.json';
+import data from '../data.json';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    ></Profile>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
